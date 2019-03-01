a52cbb14bedc4d2b88ecf5b15c2a134642b22448
fateme135 <m.fatemeh74@gmail.com>
2019-03-01 15:53:45 +0330

    step2

---
+++ models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const user = new Schema({
+    username: {
+        type: String,
+        // unique: true,
+        required: true,
+    },
+    firstname: {
+        type: String,
+        // unique: true,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        // unique: true,
+        required: true,
+    },
+ 
+    password: {
+        type: String,
+        // unique: true,
+        required: true,
+    },
+ 
+    phonenumber: {
+        type: Number,
+        required: true,
+    },
+    // sex: {
+    //     type: String,
+    //     required: true,
+    // },
+    role: {
+        type: String,
+        // unique: true,
+        required: true,
+    },
+    // Picture: {
+    //     type: String,
+    //     required: true,
+    // },
+    // code: {
+    //     type: String,
+    //     unique: true,
+    //     required: true
+    // } 
+    });
+    module.exports = mongoose.model('user', user);
